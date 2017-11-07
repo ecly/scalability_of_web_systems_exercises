0cd71e1583059a7356311d93f8866324042da3b0
ecly <ecly@mailbox.org>
2017-11-07 10:30:29 +0100

    initial tracing

--- session-05/tracing/main.go
+++ session-05/tracing/main.go
@@ -1,3 +1,4 @@
+// code from: https://github.com/jf87/scalable_web_systems/blob/master/session-05/4-tracing/1-tracing.md
 package main
 
 import (
@@ -8,5 +9,27 @@ import (
 
 func main() {
 	_ = trace.Start(os.Stdout)
-	defer trace.Stop
+	defer trace.Stop()
+
+	const n = 3
+
+	leftmost := make(chan int)
+	right := leftmost
+	left := leftmost
+
+	for i := 0; i < n; i++ {
+		right = make(chan int)
+		go pass(left, right)
+		left = right
+	}
+
+	go sendFirst(right)
+	fmt.Fprintln(os.Stderr, <-leftmost)
 }
+
+func pass(left, right chan int) {
+	v := 1 + <-right
+	left <- v
+}
+
+func sendFirst(ch chan int) { ch <- 0 }
